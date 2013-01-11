eaaca8691171077bcba7aea37242c1bf244a8d9d
d33tah <d33tah@debian.(none)>
2013-01-11 23:49:32 +0100

    initial commit

---
+++ skrypt.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import smtplib, imaplib, email
+
+"""
+Currently it's just a Kindle forwarder. Too lazy to document for now.
+"""
+
+from config import imap_host, smtp_host, smtp_port, user, passwd
+from config import from_addr, admin_addr, to_addr
+
+imap_host = "imap.gmail.com"
+smtp_host = "smtp.gmail.com"
+smtp_port = 587
+user = "d33tah.kindle"
+passwd = "nKx872ML"
+from_addr = user
+admin_addr = "d33tah@gmail.com"
+to_addr = "d33tah@kindle.com"
+
+client = imaplib.IMAP4_SSL(imap_host)
+client.login(user, passwd)
+client.select('INBOX')
+
+result, search_data = client.search(None, "ALL")
+email_ids = search_data[0].split()
+
+smtp_loggedin = False
+
+for email_id in reversed(email_ids):
+	if not smtp_loggedin:
+		smtp = smtplib.SMTP(smtp_host, smtp_port)
+		smtp.starttls()
+		smtp.login(user, passwd)
+		smtp_loggedin = True
+
+	status, data = client.fetch(email_id, "(RFC822)")
+	email_data = data[0][1]
+	message = email.message_from_string(email_data)
+
+	original_from = message['From']
+	original_subject = message['Subject']
+
+	message.replace_header("From", from_addr)
+	message.replace_header("To", to_addr)
+	del message['Message-ID']
+
+	print(message.as_string())
+	sender_domain = original_from.split('@')[1].lower()
+	if sender_domain.find('kindle')==-1 and sender_domain.find('amazon')==-1:
+		smtp.sendmail(from_addr, to_addr, message.as_string())
+		message.replace_header('Subject',"Re: %s" % original_subject)
+		message.set_payload('SENT.\n\n'+message.get_payload())
+		smtp.sendmail(from_addr, admin_addr, message.as_string())
+	else:
+		message.replace_header('Subject',"KINDLE ERROR: %s" % original_subject)
+		smtp.sendmail(from_addr, admin_addr, message.as_string())
+	client.store(email_id, '+FLAGS', '\\Deleted')
+	client.expunge()
+
+if smtp_loggedin:
+	print("...done.")
+	smtp.quit()
+else:
+	print("...did nothing.")
+
+client.close()
+client.logout()
+
